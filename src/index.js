fad7a4f2355087e5e7a925906076cd6679fd959e
onetk <info.media.tk@gmail.com>
2019-04-10 14:05:55 +0900

    add reverse func/ fix main rule

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Link } from 'react-scroll';
+// import { Link } from 'react-scroll';
 
 
 function Square(props) {
@@ -38,10 +38,15 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    var initialTable = Array(64).fill(null)
+    initialTable[27] = "○";
+    initialTable[28] = "●";
+    initialTable[35] = "●";
+    initialTable[36] = "○";
     this.state = {
       history: [
         {
-          squares: Array(64).fill(null)
+          squares: initialTable
         }
       ],
       stepNumber: 0,
@@ -53,15 +58,21 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "●" : "○";
-    this.setState({
-      history: history.concat([{ squares: squares }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
-    });
+    if (JSON.stringify(squares) === JSON.stringify(calculateTable(squares, i))) {
+      squares[i] = null
+    } else {
+      //  盤上の計算を行ったものを代入する形
+      this.setState({
+        history: history.concat([{ squares: calculateTable(squares, i) }]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext
+      });
+    }
   }
 
   jumpTo(step) {
@@ -148,4 +159,105 @@ function calculateWinner(squares) {
     return winner_side;
   }
   return null;
+}
+
+function calculateTable(squares, thisTime) {
+
+  var table = squares;
+
+  // left part
+  for (var left = 1; left <= thisTime % 8; left++) {
+    if (table[thisTime - left] == squares[thisTime]) {
+      for (let i = 1; i < left; i++) {
+        table[thisTime - i] = squares[thisTime];
+      }
+      break;
+    } else if (table[thisTime - left] == null) {
+      break;
+    }
+  }
+  //  right part
+  for (var right = 1; right <= 8 - (thisTime % 8); right++) {
+    if (table[thisTime + right] == squares[thisTime]) {
+      for (let i = 1; i < right; i++) {
+        table[thisTime + i] = squares[thisTime];
+      }
+      break;
+    } else if (table[thisTime + right] == null) {
+      break;
+    }
+  }
+  //  upper part
+  for (var upper = 1; upper <= thisTime / 8; upper++) {
+    if (table[thisTime - upper * 8] == squares[thisTime]) {
+      for (let i = 1; i < upper; i++) {
+        table[thisTime - i * 8] = squares[thisTime];
+      }
+      break;
+    } else if (table[thisTime - upper * 8] == null) {
+      break;
+    }
+  }
+  //  downer part
+  for (var downer = 1; downer <= 8 - (thisTime / 8); downer++) {
+    if (table[thisTime + downer * 8] == squares[thisTime]) {
+      for (let i = 1; i < downer; i++) {
+        table[thisTime + i * 8] = squares[thisTime];
+      }
+      break;
+    } else if (table[thisTime + downer * 8] == null) {
+      break;
+    }
+  }
+
+  //  upper right part
+  for (var upperR = 1; upperR <= thisTime / 7; upperR++) {
+    if (table[thisTime - upperR * 7] == squares[thisTime]) {
+      for (let i = 1; i < upperR; i++) {
+        table[thisTime - i * 7] = squares[thisTime];
+      }
+      break;
+    } else if (table[thisTime - upperR * 7] == null) {
+      break;
+    }
+  }
+
+  //  upper right part
+  for (var upperL = 1; upperL <= 8 - (thisTime / 9); upperL++) {
+    if (table[thisTime + upperL * 9] == squares[thisTime]) {
+      for (let i = 1; i < upperL; i++) {
+        table[thisTime + i * 9] = squares[thisTime];
+      }
+      break;
+    } else if (table[thisTime + upperL * 9] == null) {
+      break;
+    }
+  }
+
+  //  upper left part
+  for (var upperL = 1; upperL <= thisTime / 9; upperL++) {
+    if (table[thisTime - upperL * 9] == squares[thisTime]) {
+      for (let i = 1; i < upperL; i++) {
+        table[thisTime - i * 9] = squares[thisTime];
+      }
+      break;
+    } else if (table[thisTime - upperL * 9] == null) {
+      break;
+    }
+  }
+  //  downer left part
+  for (var upperR = 1; upperR <= 8 - (thisTime / 7); upperR++) {
+    if (table[thisTime + upperR * 7] == squares[thisTime]) {
+      for (let i = 1; i < upperR; i++) {
+        table[thisTime + i * 7] = squares[thisTime];
+      }
+      break;
+    } else if (table[thisTime + upperR * 7] == null) {
+      break;
+    }
+  }
+
+
+  return table;
+
 }
